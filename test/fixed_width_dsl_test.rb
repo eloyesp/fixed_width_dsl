de3415cf8e5746f057958506748fc89deb249569
Eloy Espinaco <eloyesp@gmail.com>
2016-09-28 16:48:02 -0300

    Fix tests

--- test/fixed_width_dsl_test.rb
+++ test/fixed_width_dsl_test.rb
@@ -8,14 +8,13 @@ end
 
 assert_equal sample_format.format_string, '%-20s%02i%010i'
 
-puts
-puts sample_format.debug, <<DEBUG
+assert_equal sample_format.debug, <<DEBUG
 field                | wi | fro |  to | type    | flags
 name                 | 20 |   1 |  20 | string  |   -
 age                  |  2 |  21 |  22 | integer |   0
 amount               | 10 |  23 |  32 | integer |   0
 DEBUG
 
-assert_equal sample_format.apply(name: 'roberto', amount: 12), <<FORMATTED.chomp
-roberto             0000000012
+assert_equal sample_format.apply(name: 'roberto', age: 18, amount: 12), <<FORMATTED.chomp
+roberto             180000000012
 FORMATTED
